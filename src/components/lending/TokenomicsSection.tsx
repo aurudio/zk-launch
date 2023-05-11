c545ec52a7f4871ea9f09fa2eeee6e940c238f34
Alikulov <ramoalikulov77@gmail.com>
2023-05-12 00:41:26 +0600

    merge branches

--- src/components/lending/TokenomicsSection.tsx
+++ src/components/lending/TokenomicsSection.tsx
@@ -17,32 +17,45 @@ const Tokenomics = () => {
 	]
 
 	return (
-		<section className='flex flex-col items-start px-5 relative overflow-hidden w-full'>
-			<div className='w-full max-w-7xl px-5 mx-auto z-[1]'>
+		<section className='flex flex-col items-start px-5 relative overflow-hidden w-full max-[503px]:px-0'>
+			<div className='w-full max-w-7xl  mx-auto z-[1]'>
 				<div>
-					<h2 className='text-5xl font-semibold mb-6'>Tokenomics</h2>
-					<div className='flex items-center mb-20 gap-x-[66px]'>
-						<Image src={diagram} alt='' />
-						<div className='flex flex-col w-[520px]'>
+					<h2 className='text-5xl font-semibold mb-6 max-[503px]:text-[32px] max-[503px]:ml-2'>
+						Tokenomics
+					</h2>
+					<div className='flex items-center mb-20 gap-x-[66px] max-[902px]:flex-col'>
+						<Image
+							src={diagram}
+							className='w-[47vw] max-[902px]:w-[60vw] max-[902px]:mb-12 max-[503px]:w-[85vw]'
+							alt=''
+						/>
+						<div className='flex flex-col w-[520px] max-[902px]:w-[90%]'>
 							<div className='flex flex-col gap-y-2 items-start mb-10'>
-								<h3 className='text-[32px] font-semibold'>$ZKL Token</h3>
-								<p className='text-[22px] font-normal w-full text-[#949494]'>
+								<h3 className='text-[32px] font-semibold max-[1046px]:text-[24px] max-[503px]:text-[32px]'>
+									$ZKL Token
+								</h3>
+								<p className='text-[22px] font-normal w-full text-[#949494] max-[1046px]:text-[18px]'>
 									First token bringing real utility into launchpads by offering
 									both professional customizable tools for builders and more
 									allocations with special perks for investors
 								</p>
 							</div>
-							<div className='flex flex-wrap gap-6 mb-3'>
+							<div className='flex flex-wrap gap-6 mb-3 max-[1046px]:gap-3 max-[841px]:justify-between max-[841px]:gap-y-6'>
 								{tokens.map(item => (
-									<div className='flex w-60 gap-x-2' key={item.id}>
+									<div
+										className='flex w-60 max-[1227px]:w-48 gap-x-2 max-[841px]:gap-y-20  max-[1046px]:w-40 max-[841px]:w-40 max-[503px]:w-44'
+										key={item.id}
+									>
 										<hr
 											style={{ background: item.color }}
 											className={`w-2 h-[54px] border-0 rounded-xl`}
 										/>
 										<div className='flex flex-col'>
-											<h5 className='text-2xl font-semibold'>{item.title}</h5>
+											<h5 className='text-2xl max-[1046px]:text-[14px] font-semibold max-[841px]:text-[25px] max-[503px]:text-[22px]'>
+												{item.title}
+											</h5>
 											<p
-												className={`text-lg font-normal`}
+												className={`text-lg font-normal max-[1046px]:text-[14px] max-[841px]:text-[18px]`}
 												style={{ color: item.color }}
 											>
 												{item.value} ZKL
@@ -51,12 +64,12 @@ const Tokenomics = () => {
 									</div>
 								))}
 							</div>
-							<p className='font-normal text-start text-white/50'>
+							<p className='font-normal text-start text-white/50 max-[503px]:text-sm'>
 								*DAO Treasury: bounty, marketing, ecosystem fund, etc
 							</p>
 						</div>
 					</div>
-					<div className='relative px-[92px] py-16 border border-[#1E69FF] rounded-2xl'>
+					<div className='relative px-[92px] max-[841px]:px-[80px] py-16 border border-[#1E69FF] rounded-2xl max-[503px]:hidden'>
 						<p className='uppercase absolute right-12 -top-5 py-[10.5px] px-2 border border-[#1E69FF] rounded-md bg-black'>
 							token
 						</p>
@@ -66,8 +79,10 @@ const Tokenomics = () => {
 									className='flex items-center flex-col w-[23%] gap-y-3'
 									key={item.id}
 								>
-									<h5 className='text-5xl font-semibold'>{item.title}</h5>
-									<h6 className='text-[#0066FF] text-xl font-bold'>
+									<h5 className='text-5xl font-semibold max-[841px]:text-3xl'>
+										{item.title}
+									</h5>
+									<h6 className='text-[#0066FF] text-xl font-bold max-[841px]:text-sm'>
 										{item.desc}
 									</h6>
 								</div>
