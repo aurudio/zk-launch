ffcbb190fbf25e18bca9be740370ef801ca01d7f
Alikulov <ramoalikulov77@gmail.com>
2023-05-11 21:12:38 +0600

    merge branches

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,4 +1,7 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 464a9c3 (navbar, footer adaptive)
 import Image from 'next/image'
 import twitter from '@/gray-icon-twitter.svg'
 import discord from '@/gray-icon-discord.svg'
@@ -6,6 +9,7 @@ import github from '@/gray-icon-github.svg'
 import medium from '@/gray-icon-medium.svg'
 import logo from '@/logo.svg'
 
+<<<<<<< HEAD
 const Footer = () => {
 	const icons = [
 		{ id: 1, icon: twitter },
@@ -55,13 +59,64 @@ const Footer = () => {
 				</ul>
 			</div>
 =======
+=======
+>>>>>>> 464a9c3 (navbar, footer adaptive)
 const Footer = () => {
+	const icons = [
+		{ id: 1, icon: twitter },
+		{ id: 2, icon: discord },
+		{ id: 3, icon: github },
+		{ id: 4, icon: medium },
+	]
+
+	const list = [
+		{ id: 1, title: 'About' },
+		{ id: 2, title: 'Tokenomics' },
+		{ id: 3, title: 'Roadmap' },
+		{ id: 4, title: 'Launch App' },
+		{ id: 5, title: 'FAQ' },
+	]
+
 	return (
+<<<<<<< HEAD
 		<div className='py-[69px] flex justify-center shadow-[0_-1px_0_#292A2B]'>
 			<p className='font-normal opacity-40'>
 				© 2023 zkLaunch | All rights reserved
 			</p>
 >>>>>>> 0c23346 (presale add)
+=======
+		<div className='sm:py-[69px] pt-9 pb-8 flex flex-col items-center shadow-[0_-1px_0_#292A2B]'>
+			<div className='flex justify-between items-end max-w-7xl w-full px-5 mb-[2.875rem] sm:mb-[6.375rem]'>
+				<div className='flex flex-col gap-y-4 mb-[1.125rem]'>
+					<Image src={logo} alt='' />
+					<p className='text-[#686A6D] font-normal'>
+						Built zkPunks, the first NFT collection and auction on @zkSync
+					</p>
+				</div>
+				<ul className='flex max-[1150px]:hidden'>
+					{list.map(item => (
+						<li
+							key={item.id}
+							className='text-[#686A6D] p-5 text-lg font-normal cursor-pointer'
+						>
+							{item.title}
+						</li>
+					))}
+				</ul>
+			</div>
+			<div className='flex items-center sm:justify-between sm:flex-row flex-col-reverse gap-y-12 max-w-7xl w-full px-5'>
+				<p className='font-normal opacity-40'>
+					© 2023 zkLaunch | All rights reserved
+				</p>
+				<ul className='flex gap-x-4'>
+					{icons.map(item => (
+						<li key={item.id} className='cursor-pointer'>
+							<Image src={item.icon} alt='' />
+						</li>
+					))}
+				</ul>
+			</div>
+>>>>>>> 464a9c3 (navbar, footer adaptive)
 		</div>
 	)
 }
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,12 +1,18 @@
 import Image from 'next/image'
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 464a9c3 (navbar, footer adaptive)
 import Logo from '@/logo.svg'
 import burger from '@/burger.svg'
 import { useState } from 'react'
 import closeBtn from '@/close-btn.svg'
+<<<<<<< HEAD
 =======
 import Logo from '../../../public/logo.svg'
 >>>>>>> 0c23346 (presale add)
+=======
+>>>>>>> 464a9c3 (navbar, footer adaptive)
 
 const Navbar = () => {
 	const navs = [
@@ -15,6 +21,7 @@ const Navbar = () => {
 		{ id: 3, title: 'Roadmap' },
 		{ id: 4, title: 'FAQ' },
 	]
+<<<<<<< HEAD
 <<<<<<< HEAD
 
 	const [vis, setVis] = useState(false)
@@ -89,6 +96,63 @@ const Navbar = () => {
 			</button>
 		</nav>
 >>>>>>> 0c23346 (presale add)
+=======
+
+	const [vis, setVis] = useState(false)
+
+	return (
+		<>
+			<nav className='max-w-7xl px-5 w-full shadow-[0_1px_0_#292A2B] flex items-center justify-between mx-auto'>
+				<Image src={Logo} alt='' />
+				<ul className='md:flex max-[850px]:text-sm hidden'>
+					{navs.map(item => (
+						<li className='p-5 cursor-pointer' key={item.id}>
+							{item.title}
+						</li>
+					))}
+				</ul>
+				<button className='bg-[#1E69FF] md:block hidden max-[850px]:text-sm max-[850px]: hover:btn-hover active:ring-4 active:ring-[#0055FF59] active:duration-200 duration-200 hover:duration-200 px-6 py-[14.5px] rounded-lg font-medium font'>
+					Connect wallet
+				</button>
+				<Image
+					src={burger}
+					onClick={() => setVis(true)}
+					alt=''
+					className='md:hidden cursor-pointer'
+				/>
+			</nav>
+			<div
+				className={` ${
+					vis ? 'h-[100vh] pb-5' : 'h-0'
+				} text-right overflow-hidden duration-500 fixed z-20 bg-black w-full`}
+			>
+				<div className='flex justify-between px-5'>
+					<Image src={Logo} alt='' />
+					<Image
+						src={closeBtn}
+						onClick={() => setVis(false)}
+						alt=''
+						className='opacity-50 cursor-pointer'
+					/>
+				</div>
+				<div className='relative flex justify-center w-full h-[90%]'>
+					<ul className='flex flex-col mb-6 w-full shadow-[inset0_-1px_0_#292A2B]'>
+						{navs.map(item => (
+							<li
+								className='p-5 text-left cursor-pointer font-bold shadow-[0_1px_0_#292A2B]'
+								key={item.id}
+							>
+								{item.title}
+							</li>
+						))}
+					</ul>
+					<button className='bg-[#1E69FF] max-[530px]:w-4/5 absolute bottom-0 hover:btn-hover active:ring-4 active:ring-[#0055FF59] active:duration-200 duration-200 hover:duration-200 px-6 py-[14.5px] rounded-lg font-medium font'>
+						Connect wallet
+					</button>
+				</div>
+			</div>
+		</>
+>>>>>>> 464a9c3 (navbar, footer adaptive)
 	)
 }
 
