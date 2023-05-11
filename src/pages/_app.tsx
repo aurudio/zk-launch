eb583ce377395cb2e3da96b4f89dffc6f580fe40
Alikulov <ramoalikulov77@gmail.com>
2023-05-11 21:13:18 +0600

    merge branches

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import '@/styles/globals.css'
 import { AppProps } from 'next/app'
-<<<<<<< HEAD
 import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -10,16 +9,6 @@ export default function App({ Component, pageProps }: AppProps) {
 				<title>zkLaucnh</title>
 				<link rel='icon' type='image/x-icon' href='/favicon.ico' />
 			</Head>
-=======
-
-export default function App({ Component, pageProps }: AppProps) {
-	return (
-<<<<<<< HEAD
-		<div className='font-GR'>
->>>>>>> 0c23346 (presale add)
-=======
-		<div className='font-GR bg-black text-white'>
->>>>>>> e5ed28c (commit)
 			<Component {...pageProps} />
 		</div>
 	)
